af9ebc2232f40192b5888157db1fa271bab21f12
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-02-15 20:00:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const eInput = document.querySelector(".eInput input");
-const submit = document.querySelector(".submit #add_btn");
-const listDiv = document.querySelector(".todo_content .list");
-const delete_btn = document.querySelector(".del");
-const check_btn = document.getElementById("check_btn");
+const googleAuth = document.getElementById("googleAuth");
+const create_acnt = document.getElementById("create_acnt");
+const back = document.getElementById("back");
+const form = document.querySelector("form");
+const emailInput = document.getElementById("email");
+const fullName = document.getElementById("fullName");
+const password = document.getElementById("password");
+const errorMsg = document.getElementById("error");
+
+const load_bx = document.querySelector(".load_bx");
+const load_text = document.querySelector(".load_text");
+const loading = document.querySelector(".loading");
+const check = document.getElementById("check");
+
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js"; 
-import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,63 +36,181 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore();
-const database = getDatabase(app);
-  
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
-submit.addEventListener("click", (e) => {
-    let input = eInput.value;
-    
-    let newElement = document.createElement("li");
-    console.log(newElement);
-  
+googleAuth.addEventListener("click", (e) => {
+    authorizeGoogleAuth();
+});
+async function authorizeGoogleAuth() {
+   await signInWithPopup(auth, provider)
+   .then((result) => {
+     // This gives you a Google Access Token. You can use it to access the Google API.
+     const credential = GoogleAuthProvider.credentialFromResult(result);
+     const token = credential.accessToken;
+     // The signed-in user info.
+     const user = result.user;
+     console.log(user);
+     // ...
+     load_bx.classList.add("active");
 
-    listDiv.appendChild(newElement);
-    newElement.textContent = input;
+        setTimeout(() => {
+            load_text.textContent = "Validating Form";
+        }, 1000);
+        setTimeout(() => {
+            load_text.textContent = "Checking for errors";
+        }, 4000);
+        setTimeout(() => {
+            load_text.textContent = "Assigning validation";
+        }, 6000);
+        setTimeout(() => {
+            load_text.textContent = "Submitting Form";
+        }, 8000);
+        setTimeout(() => {
+            console.log("Form has being submitted");
+            loading.style.display = "none";
+            load_text.textContent = "Form Submitted";
+            check.style.display = "block";
+        }, 10000);
+        setTimeout(() => {
+            window.location.href = "/ToDoApp/index.html";
+        }, 13000);
+   }).catch((error) => {
+     // Handle Errors here.
+     const errorCode = error.code;
+     const errorMessage = error.message;
+     console.error(errorCode);
+     // The email of the user's account used.
+     const email = error.email;
+     // The AuthCredential type that was used.
+     const credential = GoogleAuthProvider.credentialFromError(error);
+     // ...
+   });
+}
+create_acnt.addEventListener("click", (e) => {
+    formSignIn();
+});
+async function formSignIn() {
+    await document.querySelector(".wrapper").classList.add("hide");
+    await document.querySelector(".emailAuth").classList.add("show");
+}
+back.addEventListener("click", (e) => {
+    document.querySelector(".wrapper").classList.remove("hide");
+    document.querySelector(".emailAuth").classList.remove("show");
+});
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(emailInput.value == ""){
+        errorMsg.classList.add("invalid");
+        emailInput.classList.add("invalid");
+        errorMsg.textContent = "Please fill the required fields";
+    }else{
+        validate();
+    }
+    if(fullName.value == ""){
+        errorMsg.classList.add("invalid");
+        fullName.classList.add("invalid");
+        errorMsg.textContent = "Please fill the required fields";
+    }else{
+        errorMsg.classList.remove("invalid");
+        fullName.classList.remove("invalid");
+        fullName.classList.add("valid");
+    }
+    if(password.value == ""){
+        errorMsg.classList.add("invalid");
+        password.classList.add("invalid");
+        errorMsg.textContent = "Please fill the required fields";
+    }else{
+        errorMsg.classList.remove("invalid");
+        password.classList.remove("invalid");
+        checkPassword();
+    }
+    password.addEventListener("keyup", (e) => {
+        checkPassword();
+    });
+    function checkPassword() {
+    if(password.value.length < 6) {
+        password.classList.add("invalid");
+        errorMsg.classList.add("invalid");
 
-    delete_btn.style.display = 'block';
-    check_btn.style.display = 'block';
-    delete_btn.addEventListener("click", (e) => {
-      // listDiv.removeChild(newElement);
-      removeElements(document.querySelectorAll('li'));
-      function removeElements(newElement){
-        for(var i = 0; i < newElement.length; i++){
-          newElement[i].parentNode.removeChild(newElement[i]);
+        errorMsg.textContent = "Password must contain at least 6 Characters";
+    }else{
+        password.classList.remove("invalid");
+        errorMsg.classList.remove("invalid");
+        password.classList.add("valid");
+    }
+    }
+    emailInput.addEventListener("keyup", (e) => {
+        validate();
+    });
+    function validate() {
+        let regexPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if(!emailInput.value.match(regexPattern)){
+            emailInput.classList.add("invalid");
+            errorMsg.classList.add("invalid");
+            (emailInput.value != "") ? errorMsg.textContent = "Enter a valid email address" : errorMsg.textContent = "Please fill the required fields";
+        }else{
+            emailInput.classList.remove("invalid");
+            errorMsg.classList.remove("invalid");
+
+            emailInput.classList.add("valid");
         }
     }
-    delete_btn.style.display = 'none';
-    check_btn.style.display = 'none';
-    });
-    check_btn.addEventListener("click", (e) => {
-      // let text = "";
-      sta(document.querySelectorAll('li'));
-      function sta(newElement){
-        for(var i = 0; i < newElement.length; i++){
-          newElement[i].style.color = '#a6a6a6';
-          newElement[i].classList.add("active");
-        }
-      }
-    });
+    if(emailInput.classList.contains("valid") && fullName.classList.contains("valid") && password.classList.contains("valid")){
+        authenticateLogin();
 
+        load_bx.classList.add("active");
 
+        setTimeout(() => {
+            load_text.textContent = "Validating Form";
+        }, 1000);
+        setTimeout(() => {
+            load_text.textContent = "Checking for errors";
+        }, 4000);
+        setTimeout(() => {
+            load_text.textContent = "Assigning validation";
+        }, 6000);
+        setTimeout(() => {
+            load_text.textContent = "Submitting Form";
+        }, 8000);
+        setTimeout(() => {
+            console.log("Form has being submitted");
+            loading.style.display = "none";
+            load_text.textContent = "Form Submitted";
+            check.style.display = "block";
+        }, 10000);
+        setTimeout(() => {
+            window.location.href = "/ToDoApp/index.html";
+        }, 13000);
+    }
+    function authenticateLogin()  {
+    let email = emailInput.value;
+    let fullNameVal = fullName.value;
+    let passwords = password.value;
 
-    
-    // let userData = input;
-    // let getLocalStorage = localStorage.getItem("Todo");
-    // if(getLocalStorage == null) {
-      //     listArr = [];
-    // }else{
-    //     listArr == JSON.parse(getLocalStorage);
-    // }
-    // listArr.push(userData);
-    // localStorage.setItem("Todo", JSON.stringify(listArr));
+    // function to createUserCredentials and store it in an authenticate database //note that this login is a one-time-login user can't login in if the email exist
+    createUserWithEmailAndPassword(auth, email, passwords, fullNameVal)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
-    writeUserData();
-    function writeUserData(userId, name, email) {
-        const db = getDatabase();
-        set(ref(db, 'users/' + userId), {
-          Todo: eInput.value,
-          email: "",
-        });
-      }
-    });
+    //function to sign user in to the databse with their created credentials
+  signInWithEmailAndPassword(auth, email, passwords, fullNameVal)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+   }
+});
